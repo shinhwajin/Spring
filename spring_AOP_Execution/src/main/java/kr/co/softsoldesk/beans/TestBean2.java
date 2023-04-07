65b7f3f9bec3dfe4912aa2acfea85a4a355efa26
HWAJIN <HWAJIN@DESKTOP-UK4BD5L>
2023-04-07 10:31:25 +0900

    23_0407 spring_AOP_Execution

---
+++ spring_AOP_Execution/src/main/java/kr/co/softsoldesk/beans/TestBean2.java
@@ -0,0 +1,9 @@
+package kr.co.softsoldesk.beans;
+
+public class TestBean2 {
+
+	public void method1(int a1) {
+		System.out.println("beans.TestBean2.method1(int)");
+	}
+
+}
---
+++ spring_AOP_Execution/src/main/java/kr/co/softsoldesk/beans2/TestBean1.java
@@ -0,0 +1,9 @@
+package kr.co.softsoldesk.beans2;
+
+public class TestBean1 {
+
+	public void method1() {
+		System.out.println("beans2.TestBean1.method1()");
+	}
+	
+}
